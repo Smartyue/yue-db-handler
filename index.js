37d07692b533ecbce2df507902eaa52800f0f137
Yuanjianxin <1814888800@qq.com>
2018-05-17 10:49:08 +0800

    删除不必要的包

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 /**
  * Created by yuanjianxin on 2018/3/13.
  */
-const _handler=require('./lib/_handler');
-module.exports=(configs)=>{
-    _handler.config(configs);
-    return _handler.instance;
-}
+const DbHandler = require('./lib/_handler');
+module.exports = DbHandler;
